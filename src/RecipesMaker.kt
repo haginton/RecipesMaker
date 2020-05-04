eedc951fa4a2daf211feb07a27ab5ae365b3e487
Harrison Martinez <harrisonmartinez19@gmail.com>
2020-05-04 18:09:05 -0500

    Reto 2

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun main(args: Array<String>){
     var evualuar: Int
     do {
         val opciones = """
@@ -16,9 +16,11 @@ fun main(){
         evualuar = respuesta!!.toInt()
 
         if (evualuar.equals(1)){
-            println("Escogiste la opcion: $respuesta Hacer una receta")
+            //println("Escogiste la opcion: $respuesta Hacer una receta")
+            makeRecipe()
         }else if (evualuar.equals(2)){
-            println("Escogiste la opcion: $respuesta Ver mis recetas")
+            //println("Escogiste la opcion: $respuesta Ver mis recetas")
+            viewRecipe()
         }else if (evualuar < 1 || evualuar > 3){
             println("Escoge una opcion entre el 1 al 3")
         }
@@ -26,5 +28,35 @@ fun main(){
     }while (!evualuar.equals(3))
 
     println("Saliste del programa, Escogiste la opcion $evualuar")
+
+}
+
+fun makeRecipe(){
+    val categoria = """
+        
+        Hacer receta
+        Seleciona por categoría el ingrediente que buscas
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+        6. Cereal
+        7. Huevos
+        8. Aceites
+        
+    """.trimIndent()
+    println(categoria)
+}
+
+fun viewRecipe(){
+
+    val recetas = """
+        
+        Ver mis recetas
+        
+    """.trimIndent()
+
+    println(recetas)
 
 }
